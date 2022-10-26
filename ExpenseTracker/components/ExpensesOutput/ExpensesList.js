9c58853cc2bee5b99056322fd326210465c12f34
Bruno Kamada <brunokamada72@gmail.com>
2022-10-25 21:57:36 -0300

    feat: advance in the ExpenseTracker App

---
+++ ExpenseTracker/components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,18 @@
+import { FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
+
+function renderExpenseItem(itemData) {
+  return <ExpenseItem { ...itemData.item } />;
+}
+
+function ExpensesList({expenses}) {
+  return (
+    <FlatList
+      keyExtractor={(item) => item.id} 
+      data={expenses} 
+      renderItem={renderExpenseItem} 
+    />
+  );
+}
+
+export default ExpensesList;
